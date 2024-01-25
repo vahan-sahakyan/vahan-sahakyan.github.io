c183df55e74d2e608a852b6c0f66175468ce56b9
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-01-25 16:21:06 +0400

    fixed current-exp-time issue on mobile

--- js/experience.js
+++ js/experience.js
@@ -1,41 +1,22 @@
 // experience.js
 
-document.addEventListener("DOMContentLoaded", function () {
-  const startDate = new Date("Oct 2023");
-
-  // Get the current date
+document.addEventListener('DOMContentLoaded', function () {
+  const startDate = new Date('2023-10-01');
   const currentDate = new Date();
-
-  // Calculate the difference in months
   const diffInMonths =
     (currentDate.getFullYear() - startDate.getFullYear()) * 12 + (currentDate.getMonth() - startDate.getMonth()) + 1;
-
-  // Display the experience time in months
-  const currentExpTimeElement = document.getElementById("current-exp-time");
-  if (currentExpTimeElement) {
-    currentExpTimeElement.textContent = formatExperienceTime(diffInMonths);
-  }
+  const currentExpTimeElement = document.getElementById('current-exp-time');
+  if (currentExpTimeElement) currentExpTimeElement.textContent = formatExperienceTime(diffInMonths);
 });
 
 // Function to format the experience time
 function formatExperienceTime(months) {
   const years = Math.floor(months / 12);
   const remainingMonths = months % 12;
-
-  if (years === 0 && remainingMonths === 0) {
-    return "Less than a month";
-  }
-
-  const yearsString = years > 0 ? `${years} ${years === 1 ? "yr" : "yrs"}` : "";
-  const monthsString = remainingMonths > 0 ? `${remainingMonths} ${remainingMonths === 1 ? "mo" : "mos"}` : "";
-
-  // Handle the case where only years or only months are present
-  if (years > 0 && remainingMonths === 0) {
-    return yearsString;
-  } else if (years === 0 && remainingMonths > 0) {
-    return monthsString;
-  }
-
-  // Handle the case where both years and months are present
+  if (years === 0 && remainingMonths === 0) return '1 mo';
+  const yearsString = years > 0 ? `${years} ${years === 1 ? 'yr' : 'yrs'}` : '';
+  const monthsString = remainingMonths > 0 ? `${remainingMonths} ${remainingMonths === 1 ? 'mo' : 'mos'}` : '';
+  if (years > 0 && remainingMonths === 0) return yearsString;
+  else if (years === 0 && remainingMonths > 0) return monthsString;
   return `${yearsString} ${monthsString}`;
 }
