7e41f3aa9ea13e97b5f2cfcf19998dfa137014d4
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-08-03 19:20:58 +0400

    add exp - forbes, op, wmg

--- js/experience.js
+++ js/experience.js
@@ -1,22 +1,29 @@
 // experience.js
 
 document.addEventListener("DOMContentLoaded", function () {
-  const startDate = new Date("2023-09-01");
+  const startDate = new Date("2024-05-01");
   const currentDate = new Date();
   const diffInMonths =
     (currentDate.getFullYear() - startDate.getFullYear()) * 12 + (currentDate.getMonth() - startDate.getMonth()) + 1;
-  const currentExpTimeElement = document.getElementById("current-exp-time");
-  if (currentExpTimeElement) currentExpTimeElement.textContent = formatExperienceTime(diffInMonths);
+  const currentExpTimeElements = document.querySelectorAll(".current-exp-time");
+  if (currentExpTimeElements) {
+    currentExpTimeElements.forEach((el) => (el.textContent = formatExperienceTime(diffInMonths)));
+  }
 });
 
 // Function to format the experience time
 function formatExperienceTime(months) {
   const years = Math.floor(months / 12);
   const remainingMonths = months % 12;
-  if (years === 0 && remainingMonths === 0) return "1 mo";
+  if (years === 0 && remainingMonths === 0) {
+    return "1 mo";
+  }
   const yearsString = years > 0 ? `${years} ${years === 1 ? "yr" : "yrs"}` : "";
   const monthsString = remainingMonths > 0 ? `${remainingMonths} ${remainingMonths === 1 ? "mo" : "mos"}` : "";
-  if (years > 0 && remainingMonths === 0) return yearsString;
-  else if (years === 0 && remainingMonths > 0) return monthsString;
+  if (years > 0 && remainingMonths === 0) {
+    return yearsString;
+  } else if (years === 0 && remainingMonths > 0) {
+    return monthsString;
+  }
   return `${yearsString} ${monthsString}`;
 }
